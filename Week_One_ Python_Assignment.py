bee6112113cc184b9a9718736ad0c1b5938bf49f
Abbywealth23 <abbywealth23@gmail.com>
2025-07-28 18:27:24 +0100

    made few lines of new comments

--- Week_One_ Python_Assignment.py
+++ Week_One_ Python_Assignment.py
@@ -6,22 +6,22 @@ operator = (input("Enter the operator (-, +, x, /): "))
 
 #calculations 
 if operator == "+":
-        add = num1 + num2   #addition
+        add = num1 + num2   #if the user enters the + operation, this line performs the addition arithmetic
         print(f"{num1} + {num2} = {add}")
         print("Done!")
 elif operator == "-":
-        subtract = num1 - num2  #subtraction
+        subtract = num1 - num2  #if the user enters the - operation, this line performs the subtraction arithmetic
         print(f"{num1} - {num2} = {subtract}")
         print("Done")
 elif operator == "/":
         if num2 != 0:
-                division = num1 / num2  #Division  
+                division = num1 / num2  #if the user enters the / operation, this line performs the Division arithmetic
                 print(f"{num1} / {num2} = {division}")   
                 print("Done")
         else:
                 print("sorry, you can't divide by 0")
 elif operator == "*":
-        multiply = num1 * num2  #multiplication
+        multiply = num1 * num2  #if the user enters the * operation, this line performs the multiplication arithmetic
         print(f"{num1} * {num2} = {multiply}")
         print("Done")
 else: print("Oops! Wrong Operator")
